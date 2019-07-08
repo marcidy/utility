367f769549d52cec1c12814197eb22c8cb4538fb
Jay Querie <jay@querie.cc>
2019-07-08 01:14:12 -0700

    decent graphs

--- WebServerAP/web-ui/src/index.js
+++ WebServerAP/web-ui/src/index.js
@@ -12,10 +12,10 @@ const smoothie_power = new SmoothieChart();
 const pwm_line = new TimeSeries();
 const anmeter_line = new TimeSeries();
 
-smoothie_water.addTimeSeries(anmeter_line, { lineWidth:2, strokeStyle:'#0000ff' });
+smoothie_water.addTimeSeries(anmeter_line, { labels:{disabled:true}, lineWidth:2, strokeStyle:'#0000ff' });
 smoothie_water.streamTo(canvas_water, 500);
 
-smoothie_power.addTimeSeries(pwm_line, { lineWidth: 2, strokeStyle: '#00ff00' });
+smoothie_power.addTimeSeries(pwm_line, { labels:{disabled:true}, lineWidth: 2, strokeStyle: '#00ff00' });
 smoothie_power.streamTo(canvas_power, 500);
 
 resize();
@@ -24,6 +24,9 @@ function resize() {
 	canvas_water.setAttribute('width', window.innerWidth);
 	canvas_power.setAttribute('width', window.innerWidth);
 }
+
+let prev_pwm = 0;
+let prev_an = 0;
 
 socket.onmessage = function(event) {
 	out.textContent = String(event.data);
@@ -39,8 +42,17 @@ socket.onmessage = function(event) {
 	const pwm = data.pwm;
 	const anmeter = data.anmeter;
 	const now = new Date().getTime(); // TODO: replace with time from ESP chip *!*
-	pwm_line.append(now, pwm);
-	anmeter_line.append(now, anmeter);
+
+	if (Math.abs(prev_pwm - pwm) > 1) {
+	  pwm_line.append(now, Math.abs(pwm));
+	}
+
+	if (Math.abs(prev_an - anmeter) > 1) {
+	  anmeter_line.append(now, Math.abs(anmeter));
+	}
+
+	prev_pwm = pwm;
+	prev_an = anmeter;
 }
 
 
